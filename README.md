a5227ff30ed0c1c8890e17afe800a63763d625a9
Elizabeth dos Santos <62735673+elizabethesantos@users.noreply.github.com>
2025-10-11 05:17:47 +0000

    adiciona scripts `lint:check`e`lint:fix`

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # clone-tabnews
-Implementação do https://www.tabnews.cvom.br para o https://curso.dev
 
+Implementação do https://www.tabnews.cvom.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Para vc meu amor, que é muito especial para mim, Eu Te Amo!!!!</h1>
+  return (
+    <h1>Para vc meu amor, que é muito especial para mim, Eu Te Amo!!!!</h1>
+  );
 }
 
 export default Home;
